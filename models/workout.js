4ac21eab4a8487d1b7098e7a9ad6ce6873263e74
Tomek Regulski <75456264+tomekregulski@users.noreply.github.com>
2021-05-23 18:34:22 -0400

    initial build and server connection

---
+++ models/workout.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  name: {
+    type: String,
+    trim: true,
+    required: "Enter a name for transaction"
+  }
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
